b173f434aeb5c46d0d891fb5cc9e15f8fbdf1f1a
Christopher Demahy <demahyc@gmail.com>
2020-08-14 19:25:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,11 @@
 # Write your methods here
 def loop_message_five_times(message)
   5.times do
-  puts message
+    puts message
   end
 end
-def loop_message_n_times()
+def loop_message_n_times(message, number)
+  number.times do
+    puts message
+  end
 end
