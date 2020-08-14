f8ecb2c70a58acb27de67e019e918999b69eec12
Christopher Demahy <demahyc@gmail.com>
2020-08-14 19:26:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,11 +1,19 @@
-# Write your methods here
+# Loop methods practice
 def loop_message_five_times(message)
   5.times do
     puts message
   end
 end
+
 def loop_message_n_times(message, number)
   number.times do
     puts message
   end
 end
+
+def output_array(array)
+  counter = 0
+  while array[counter] do
+    puts array[counter]
+    counter++
+  end 
